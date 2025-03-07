35d50a1830c69b8edaff2eaa68925726605034e3
Fcbhaha <fuchaobao@outlook.com>
2025-03-07 12:48:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # 修改默认IP
- sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
- curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+ #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
